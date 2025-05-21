a064616f4557e84c7139f31550b2298b178dc288
Azfar <azfarrazzaq23@gmail.com>
2025-05-21 16:23:47 +0500

    removed unecessary functions from utils/functions.ts file

--- app/utils/functions.ts
+++ app/utils/functions.ts
@@ -2,9 +2,7 @@ import clientData from "~/data/clients.json";
 import services from "~/data/services.json";
 import employees from "~/data/employees.json";
 import record from "~/data/records.json";
-import type { FormType } from "./types";
-import * as fs from "fs";
-import * as path from "path";
+
 const getClientbyMobileNumber = (mobileNumberParam: string) => {
   const client = clientData.find(
     (client) => client.mobile_number === mobileNumberParam
@@ -28,28 +26,6 @@ const getEmployee = (id: string) => {
   return employees.find((employee) => employee.id === id);
 };
 
-const createServiceRecord = async (formData: FormType) => {
-  // Get existing records
-  const filePath = path.join(process.cwd(), "app/data/records.json");
-  const currentRecords = [...record];
-
-  // Generate new ID
-  const newId =
-    currentRecords.length > 0
-      ? Math.max(...currentRecords.map((record) => record.id)) + 1
-      : 1;
-
-  // const newId = 17
-  // Add new record with ID
-  const recordWithId = { ...formData, id: newId };
-  currentRecords.push(recordWithId);
-
-  // Write back to file
-  fs.writeFileSync(filePath, JSON.stringify(currentRecords, null, 2));
-
-  return { id: 17, success: true, error_msg: null };
-};
-
 const getServiceRecord = (id: string | number) => {
   return record.find((rec) => rec.id === Number(id));
 };
@@ -60,6 +36,5 @@ export {
   getService,
   getAllServices,
   getAllEmployees,
-  createServiceRecord,
   getServiceRecord,
 };
